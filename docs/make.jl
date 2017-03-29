9a1cc59598a155918b8acaffc9a595ca3323a0e2
Dan Segal <danSegal2@gmail.com>
2017-03-29 16:10:38 -0400

    Update doc generation files

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,17 @@
 using Documenter, Tokamak
 
 makedocs(
+  modules = [Tokamak],
   format = :html,
-  sitename = "Tokamak.jl"
+  sitename = "Tokamak.jl",
+  pages = Any[
+    "Home" => "index.md",
+  ]
 )
 
 deploydocs(
-  repo   = "github.com/djsegal/Tokamak.jl.git"
+  repo = "github.com/djsegal/Tokamak.jl.git",
+  target = "build",
+  deps = nothing,
+  make = nothing
 )
