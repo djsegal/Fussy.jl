b8f4ea722c8359db2223173baa07b9bfaa434ed7
Dan Segal <danSegal2@gmail.com>
2017-04-02 01:37:53 -0400

    Add test for main driving function

---
+++ test/functions/main_test.jl
@@ -0,0 +1,21 @@
+@testset "Main Function Tests" begin
+
+  @test isdefined(Tokamak, :main) == true
+
+  originalSTDOUT = STDOUT
+
+  (outRead, outWrite) = redirect_stdout()
+
+  Tokamak.main()
+
+  close(outWrite)
+
+  data = readavailable(outRead)
+
+  close(outRead)
+
+  redirect_stdout(originalSTDOUT)
+
+  @test String(data) == "done.\n"
+
+end
