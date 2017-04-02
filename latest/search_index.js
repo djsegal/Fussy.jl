ce15c02a70b7dff0df9eeab4ee8a8d3f05d659af
autodocs <autodocs>
2017-04-02 05:39:48 +0000

    build based on be75888

--- latest/search_index.js
+++ latest/search_index.js
@@ -17,336 +17,400 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Tokamak.E_Li",
-    "page": "Home",
-    "title": "Tokamak.E_Li",
-    "category": "Constant",
-    "text": "E_Li\n\nLorem ipsum dolor sit amet.\n\n\n\n"
+    "location": "code.html#",
+    "page": "Code",
+    "title": "Code",
+    "category": "page",
+    "text": ""
 },
 
 {
-    "location": "index.html#Tokamak.E_alpha",
-    "page": "Home",
-    "title": "Tokamak.E_alpha",
-    "category": "Constant",
-    "text": "E_alpha\n\nLorem ipsum dolor sit amet.\n\n\n\n"
+    "location": "code.html#Code-1",
+    "page": "Code",
+    "title": "Code",
+    "category": "section",
+    "text": ""
 },
 
 {
-    "location": "index.html#Tokamak.E_n",
-    "page": "Home",
-    "title": "Tokamak.E_n",
-    "category": "Constant",
-    "text": "E_n\n\nLorem ipsum dolor sit amet.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Tokamak.m_d",
-    "page": "Home",
-    "title": "Tokamak.m_d",
-    "category": "Constant",
-    "text": "m_d\n\nLorem ipsum dolor sit amet.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Tokamak.m_t",
-    "page": "Home",
-    "title": "Tokamak.m_t",
-    "category": "Constant",
-    "text": "m_t\n\nLorem ipsum dolor sit amet.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Tokamak.C_B-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.C_B-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.C_B",
     "category": "Method",
     "text": "C_B()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.E_F-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.E_F-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.E_F",
     "category": "Method",
     "text": "E_F()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.I_CD-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.I_CD-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.I_CD",
     "category": "Method",
     "text": "I_CD()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.K_B-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.K_B-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.K_B",
     "category": "Method",
     "text": "K_B()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.K_CD-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.K_CD-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.K_CD",
     "category": "Method",
     "text": "K_CD()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.K_F-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.K_F-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.K_F",
     "category": "Method",
     "text": "K_F()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.K_L-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.K_L-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.K_L",
     "category": "Method",
     "text": "K_L()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.K_R-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.K_R-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.K_R",
     "category": "Method",
     "text": "K_R()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.K_kappa-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.K_kappa-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.K_kappa",
     "category": "Method",
     "text": "K_kappa()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.K_rad-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.K_rad-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.K_rad",
     "category": "Method",
     "text": "K_rad()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.P_F-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.P_F-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.P_F",
     "category": "Method",
     "text": "P_F()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.P_H-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.P_H-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.P_H",
     "category": "Method",
     "text": "P_H()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.P_T-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.P_T-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.P_T",
     "category": "Method",
     "text": "P_T()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.P_alpha-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.P_alpha-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.P_alpha",
     "category": "Method",
     "text": "P_alpha()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.P_kappa-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.P_kappa-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.P_kappa",
     "category": "Method",
     "text": "P_kappa()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.P_n-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.P_n-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.P_n",
     "category": "Method",
     "text": "P_n()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.P_rad-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.P_rad-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.P_rad",
     "category": "Method",
     "text": "P_rad()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.Q_E-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.Q_E-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.Q_E",
     "category": "Method",
     "text": "Q_E()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.a-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.a-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.a",
     "category": "Method",
     "text": "a()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.a_bar-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.a_bar-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.a_bar",
     "category": "Method",
     "text": "a_bar()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.b_theta-Tuple{Any}",
-    "page": "Home",
+    "location": "code.html#Tokamak.b_theta-Tuple{Any}",
+    "page": "Code",
     "title": "Tokamak.b_theta",
     "category": "Method",
     "text": "b_theta(rho)\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.f_B-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.f_B-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.f_B",
     "category": "Method",
     "text": "f_B()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.f_CD-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.f_CD-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.f_CD",
     "category": "Method",
     "text": "f_CD()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.f_RP-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.f_RP-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.f_RP",
     "category": "Method",
     "text": "f_RP()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.greenwald_limit-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.greenwald_limit-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.greenwald_limit",
     "category": "Method",
     "text": "greenwald_limit()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.lawson_criterion-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.lawson_criterion-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.lawson_criterion",
     "category": "Method",
     "text": "lawson_criterion()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.m_r-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.m_r-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.m_r",
     "category": "Method",
     "text": "m_r()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.power_balance-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.power_balance-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.power_balance",
     "category": "Method",
     "text": "power_balance()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.power_sinks-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.power_sinks-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.power_sinks",
     "category": "Method",
     "text": "power_sinks()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.power_sources-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.power_sources-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.power_sources",
     "category": "Method",
     "text": "power_sources()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.sigma_v-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.sigma_v-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.sigma_v",
     "category": "Method",
     "text": "sigma_v()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.sigma_v_ave-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.sigma_v_ave-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.sigma_v_ave",
     "category": "Method",
     "text": "sigma_v_ave()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.sigma_v_hat-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.sigma_v_hat-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.sigma_v_hat",
     "category": "Method",
     "text": "sigma_v_hat()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.simplified_density-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.simplified_density-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.simplified_density",
     "category": "Method",
     "text": "simplified_density()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Tokamak.steady_state-Tuple{}",
-    "page": "Home",
+    "location": "code.html#Tokamak.steady_state-Tuple{}",
+    "page": "Code",
     "title": "Tokamak.steady_state",
     "category": "Method",
     "text": "steady_state()\n\nLorem ipsum dolor sit amet.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Code-1",
-    "page": "Home",
-    "title": "Code",
+    "location": "code.html#Functions-1",
+    "page": "Code",
+    "title": "Functions",
     "category": "section",
-    "text": "Modules = [Tokamak]"
+    "text": "Modules = [Tokamak]\nOrder = [:function]"
 },
 
 {
-    "location": "index.html#Index-1",
-    "page": "Home",
+    "location": "code.html#Types-1",
+    "page": "Code",
+    "title": "Types",
+    "category": "section",
+    "text": "Modules = [Tokamak]\nOrder = [:type]"
+},
+
+{
+    "location": "code.html#Tokamak.E_Li",
+    "page": "Code",
+    "title": "Tokamak.E_Li",
+    "category": "Constant",
+    "text": "E_Li\n\nLorem ipsum dolor sit amet.\n\n\n\n"
+},
+
+{
+    "location": "code.html#Tokamak.E_alpha",
+    "page": "Code",
+    "title": "Tokamak.E_alpha",
+    "category": "Constant",
+    "text": "E_alpha\n\nLorem ipsum dolor sit amet.\n\n\n\n"
+},
+
+{
+    "location": "code.html#Tokamak.E_n",
+    "page": "Code",
+    "title": "Tokamak.E_n",
+    "category": "Constant",
+    "text": "E_n\n\nLorem ipsum dolor sit amet.\n\n\n\n"
+},
+
+{
+    "location": "code.html#Tokamak.m_d",
+    "page": "Code",
+    "title": "Tokamak.m_d",
+    "category": "Constant",
+    "text": "m_d\n\nLorem ipsum dolor sit amet.\n\n\n\n"
+},
+
+{
+    "location": "code.html#Tokamak.m_t",
+    "page": "Code",
+    "title": "Tokamak.m_t",
+    "category": "Constant",
+    "text": "m_t\n\nLorem ipsum dolor sit amet.\n\n\n\n"
+},
+
+{
+    "location": "code.html#Constants-1",
+    "page": "Code",
+    "title": "Constants",
+    "category": "section",
+    "text": "Modules = [Tokamak]\nOrder = [:constant]"
+},
+
+{
+    "location": "code.html#Macros-1",
+    "page": "Code",
+    "title": "Macros",
+    "category": "section",
+    "text": "Modules = [Tokamak]\nOrder = [:macro]"
+},
+
+{
+    "location": "code.html#Modules-1",
+    "page": "Code",
+    "title": "Modules",
+    "category": "section",
+    "text": "Modules = [Tokamak]\nOrder = [:module]"
+},
+
+{
+    "location": "glossary.html#",
+    "page": "Glossary",
+    "title": "Glossary",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "glossary.html#Glossary-1",
+    "page": "Glossary",
+    "title": "Glossary",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "glossary.html#Index-1",
+    "page": "Glossary",
     "title": "Index",
     "category": "section",
     "text": ""
