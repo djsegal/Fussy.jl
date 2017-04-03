35be223553b632ae6b2f2766491c498fba373b30
Dan Segal <danSegal2@gmail.com>
2017-04-03 17:51:57 -0400

    Add a few tests that were missing from fusion vars

--- test/functions/fusion/P_F_test.jl
+++ test/functions/fusion/P_F_test.jl
@@ -4,4 +4,18 @@
 
   @test Tokamak.P_F() != Nullable
 
+  actual_value = Tokamak.P_F()
+
+  actual_value /= 1u"MW"
+
+  actual_value /= ( Tokamak.symbol_dict["R_0"] ) ^ 3
+  actual_value /= ( Tokamak.symbol_dict["n_bar"] ) ^ 2
+  actual_value /= ( Tokamak.symbol_dict["sigma_v_hat"] )
+
+  actual_value = SymPy.N(actual_value)
+
+  expected_value = Tokamak.K_F()
+
+  @test isapprox(expected_value, actual_value)
+
 end
--- test/functions/fusion/sigma_v_test.jl
+++ test/functions/fusion/sigma_v_test.jl
@@ -4,4 +4,20 @@
 
   @test Tokamak.sigma_v() != Nullable
 
+  actual_value = Tokamak.sigma_v()
+  actual_value *= 10^21
+
+  actual_value /= Tokamak.calculate_sigma_v_hat()
+
+  expected_value = 1
+
+  T_k_symbol = Tokamak.symbol_dict["T_k"]
+
+  for cur_T_k in [1e1, 1e2, 1e3]
+    cur_value = subs(actual_value, T_k_symbol, cur_T_k)
+    cur_value = SymPy.N( cur_value )
+
+    @test isapprox(cur_value, expected_value)
+  end
+
 end
