1e5610d1351a8c1fc2cbd50169b5f71c58760524
Dan Segal <danSegal2@gmail.com>
2017-04-03 18:36:21 -0400

    Rename “K_C_B” variable back to “C_B”

--- src/functions/plasmas/C_B.jl
+++ src/functions/plasmas/C_B.jl
@@ -1,9 +1,9 @@
 """
-    K_C_B()
+    C_B()
 
 Lorem ipsum dolor sit amet.
 """
-function K_C_B()
+function C_B()
   cur_func = function (rho)
     numerator = rho ^ (5/2)
     numerator *= ( 1 - rho^2 ) ^ ( nu_n + nu_T - 1 )
@@ -12,10 +12,10 @@ function K_C_B()
     numerator / denominator
   end
 
-  cur_K_C_B = ( 1 + nu_n )
-  cur_K_C_B *= ( 1 + nu_T )
-  cur_K_C_B *= ( nu_n + 0.055 * nu_T )
-  cur_K_C_B *= QuadGK.quadgk(cur_func, 0, 1)[1]
+  cur_C_B = ( 1 + nu_n )
+  cur_C_B *= ( 1 + nu_T )
+  cur_C_B *= ( nu_n + 0.055 * nu_T )
+  cur_C_B *= QuadGK.quadgk(cur_func, 0, 1)[1]
 
-  cur_K_C_B
+  cur_C_B
 end
--- test/functions/coeffs/K_C_B_test.jl
+++
@@ -1,7 +0,0 @@
-@testset "K C B Function Tests" begin
-
-  @test isdefined(Tokamak, :K_C_B) == true
-
-  @test isapprox( Tokamak.K_C_B() , 0.2244 , rtol=1e-4 )
-
-end
