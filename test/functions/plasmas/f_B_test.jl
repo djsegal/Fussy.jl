0b9e2875aa36f4c83a7093e19125e9186ba4efa1
Dan Segal <danSegal2@gmail.com>
2017-04-03 18:37:24 -0400

    Add tests for f_B and f_CD using symbolic logic

--- test/functions/plasmas/f_B_test.jl
+++ test/functions/plasmas/f_B_test.jl
@@ -4,4 +4,18 @@
 
   @test Tokamak.f_B() != Nullable
 
+  actual_value = Tokamak.f_B()
+
+  actual_value /= Tokamak.symbol_dict["n_bar"]
+  actual_value /= Tokamak.symbol_dict["T_k"]
+  actual_value /= Tokamak.symbol_dict["R_0"] ^ 2
+
+  actual_value *= Tokamak.symbol_dict["I_M"] ^ 2
+
+  actual_value = SymPy.N(actual_value)
+
+  expected_value = Tokamak.K_B()
+
+  @test isapprox(expected_value, actual_value)
+
 end
--- test/functions/plasmas/f_CD_test.jl
+++ test/functions/plasmas/f_CD_test.jl
@@ -4,4 +4,18 @@
 
   @test Tokamak.f_CD() != Nullable
 
+  actual_value = Tokamak.f_CD()
+
+  actual_value /= Tokamak.symbol_dict["n_bar"]
+  actual_value /= Tokamak.symbol_dict["R_0"] ^ 2
+  actual_value /= Tokamak.symbol_dict["sigma_v_hat"]
+
+  actual_value *= Tokamak.symbol_dict["I_M"]
+
+  actual_value = SymPy.N(actual_value)
+
+  expected_value = Tokamak.K_CD()
+
+  @test isapprox(expected_value, actual_value)
+
 end
