830f6eabb08cdc576152e8e2e70a3cd8b4077bfe
Dan Segal <danSegal2@gmail.com>
2017-04-04 01:14:20 -0400

    Standardize how some tests are written

--- test/functions/fusion/P_kappa_test.jl
+++ test/functions/fusion/P_kappa_test.jl
@@ -2,11 +2,11 @@
 
   @test isdefined(Tokamak, :P_kappa) == true
 
-  expected_value = Tokamak.K_kappa()
-
   actual_value = Tokamak.P_kappa()
-  actual_value /= ( Tokamak.T_k / 1u"keV" )
+
   actual_value /= 1u"MW"
+
+  actual_value /= ( Tokamak.T_k / 1u"keV" )
 
   actual_value /= Tokamak.symbol_dict["R_0"] ^ 3
   actual_value /= Tokamak.symbol_dict["n_bar"]
@@ -14,6 +14,8 @@
 
   actual_value = SymPy.N(actual_value)
 
-  @test isapprox( expected_value , actual_value )
+  expected_value = Tokamak.K_kappa()
+
+  @test isapprox(expected_value, actual_value)
 
 end
--- test/functions/fusion/sigma_v_test.jl
+++ test/functions/fusion/sigma_v_test.jl
@@ -9,9 +9,9 @@
 
   actual_value /= Tokamak.calculate_sigma_v_hat()
 
-  expected_value = 1
-
   T_k_symbol = Tokamak.symbol_dict["T_k"]
+
+  expected_value = 1
 
   for cur_T_k in [1e1, 1e2, 1e3]
     cur_value = subs(actual_value, T_k_symbol, cur_T_k)
