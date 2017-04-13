7043d76db1d3c4b74c86f1b8ae3ba4f4a5ba5007
Dan Segal <danSegal2@gmail.com>
2017-04-13 16:36:35 -0400

    Update I_CD to form that has a test for it

--- src/functions/plasmas/I_CD.jl
+++ src/functions/plasmas/I_CD.jl
@@ -5,11 +5,13 @@ Lorem ipsum dolor sit amet.
 """
 function I_CD()
   cur_I_CD = eta_CD
-  cur_I_CD /= Q
 
-  cur_I_CD *= P_F()
-  cur_I_CD /= n_bar
+  cur_I_CD *= ( P_H() / 1u"MW" )
+
+  cur_I_CD /= ( n_bar / 1u"n20" )
   cur_I_CD /= ( R_0 / 1u"m" )
+
+  cur_I_CD *= 1u"MA"
 
   cur_I_CD
 end
--- test/functions/plasmas/I_CD_test.jl
+++ test/functions/plasmas/I_CD_test.jl
@@ -2,6 +2,19 @@
 
   @test isdefined(Tokamak, :I_CD) == true
 
-  @test Tokamak.I_CD() != Nullable
+  actual_value = Tokamak.I_CD()
+
+  actual_value /= 1u"MA"
+
+  actual_value /= ( Tokamak.P_F() / 1u"MW" )
+
+  actual_value *= Tokamak.Q
+
+  actual_value *= Tokamak.symbol_dict["n_bar"]
+  actual_value *= Tokamak.symbol_dict["R_0"]
+
+  expected_value = Tokamak.eta_CD
+
+  @test isapprox(expected_value, actual_value, rtol=5e-4 )
 
 end
