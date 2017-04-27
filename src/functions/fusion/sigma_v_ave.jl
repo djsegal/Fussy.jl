5b2efa1099b5cfa97576c665b1d7d9d34dbf882f
Dan Segal <danSegal2@gmail.com>
2017-04-27 17:38:28 -0400

    Switch back to using exp(log(...)) fit for sigma_v

--- src/functions/fusion/sigma_v_ave.jl
+++ src/functions/fusion/sigma_v_ave.jl
@@ -6,15 +6,12 @@ Lorem ipsum dolor sit amet.
 function sigma_v_ave(rho)
 
   l_fit = [
-    +8.47607337488889,
-    -15.0271611662417,
-    +3.54811022268999,
-    -0.113845397989482,
-    +1.42248439810972e-3,
-    -6.27906321523389e-6
+    -60.4593,
+    +6.1371,
+    -0.8609,
+    +0.0356,
+    -0.0045
   ]
-
-  l_fit *= 1e-24
 
   cur_param = 1
   cur_param -= rho ^ 2
@@ -23,16 +20,18 @@ function sigma_v_ave(rho)
   cur_param *= ( T_k / 1u"keV" )
   cur_param *= 1 + nu_T
 
-  cur_sigma_v_ave = l_fit[1]
+  cur_param = log( cur_param )
 
-  for i in 1:5
+  cur_sigma_v_ave = 0
+
+  for i in 0:4
     cur_term = l_fit[i+1]
     cur_term *= cur_param ^ i
 
     cur_sigma_v_ave += cur_term
   end
 
-  cur_sigma_v_ave = max(0, cur_sigma_v_ave)
+  cur_sigma_v_ave = exp( cur_sigma_v_ave )
 
   cur_sigma_v_ave *= 1u"m^3/s"
 
