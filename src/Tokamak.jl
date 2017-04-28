c7aecb0d5d74c074d3ea0e69976473b351759061
Dan Segal <danSegal2@gmail.com>
2017-04-27 20:36:12 -0400

    Add main function for getting R & B as func of T_k

--- src/Tokamak.jl
+++ src/Tokamak.jl
@@ -3,12 +3,76 @@ module Tokamak
   using Julz
   using QuadGK
   using SymPy
+  using ProgressMeter
 
   if ( endswith(pwd(), "/test") ) ; cd("..") ; end
 
   include("../config/bootload.jl")
 
-  function main()
+  function main(num_points=50)
+    cur_R_0 = symbol_dict["R_0"]
+    cur_B_0 = symbol_dict["B_0"]
+    cur_T_k = symbol_dict["T_k"]
+
+    ignition_relation = r_b_eq_hard_coded()
+
+    solved_equations = [
+      r_b_eq_from_beta_limit,
+      r_b_eq_from_wall_loading,
+      r_b_eq_from_heat_loading
+    ]
+
+    solved_R_0_s = map(
+      cur_eq -> solve(cur_eq(), cur_R_0)[1],
+      solved_equations
+    )
+
+    solved_ignitions = map(
+      solved_R_0 -> subs(ignition_relation, cur_R_0, solved_R_0)
+      , solved_R_0_s)
+
+    T_list = logspace(0,2,num_points)
+
+    R_0_lists = [ [] for i=1:length(solved_equations) ]
+    B_0_lists = [ [] for i=1:length(solved_equations) ]
+
+    @showprogress 1 "Computing..." for cur_T in T_list
+      Tokamak.load_input( "T_k = $(cur_T)u\"keV\"" )
+
+      cur_ignitions = map(
+        solved_ignition -> calc_possible_values(solved_ignition),
+        solved_ignitions
+      )
+
+      solved_B_0_s = map(
+        cur_ignition -> solve(cur_ignition, cur_B_0),
+        cur_ignitions
+      )
+
+      for (cur_index, solved_B_0) in enumerate(solved_B_0_s)
+        if length(solved_B_0) == 0
+          continue
+        end
+
+        solved_B_0 = solved_B_0[1]
+
+        solved_R_0 = calc_possible_values(
+          subs(solved_R_0_s[cur_index], cur_B_0, solved_B_0)
+        )
+
+        push!(R_0_lists[cur_index], solved_R_0)
+        push!(B_0_lists[cur_index], solved_B_0)
+      end
+    end
+
+    for (cur_index, solved_eq) in enumerate(solved_equations)
+      println("\n$(solved_eq)\n")
+      println("R_0 = ")
+      println(R_0_lists[cur_index])
+      println("B_0 = ")
+      println(B_0_lists[cur_index])
+    end
+
     println("done.")
   end
 
--- test/functions/main_test.jl
+++ test/functions/main_test.jl
@@ -2,11 +2,17 @@
 
   @test isdefined(Tokamak, :main) == true
 
+  err_data = "nil"
+  out_data = "nil"
+  did_break = false
+
+  originalSTDERR = STDERR
   originalSTDOUT = STDOUT
 
+  (errRead, errWrite) = redirect_stderr()
   (outRead, outWrite) = redirect_stdout()
 
-  Tokamak.main()
+  Tokamak.main(2)
 
   close(outWrite)
 
@@ -14,8 +20,18 @@
 
   close(outRead)
 
+  close(errWrite)
+  close(outWrite)
+
+  err_data = readavailable(errRead)
+  out_data = readavailable(outRead)
+
+  close(errRead)
+  close(outRead)
+
+  redirect_stderr(originalSTDERR)
   redirect_stdout(originalSTDOUT)
 
-  @test String(data) == "done.\n"
+  @test contains(String(data), "done.\n")
 
 end
