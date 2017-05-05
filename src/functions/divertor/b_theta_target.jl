9941b5e3b2074ad5d2c1a9cb20e1ddd0b69f774c
Dan Segal <danSegal2@gmail.com>
2017-05-05 14:51:02 -0400

    Fix bug where b_theta abs() values seperately

--- src/functions/divertor/b_theta_target.jl
+++ src/functions/divertor/b_theta_target.jl
@@ -6,8 +6,10 @@ Lorem ipsum dolor sit amet.
 function b_theta_target()
   cur_angle = target_angle()
 
-  cur_b_theta = 0.040387 * abs(sin(cur_angle))
-  cur_b_theta += 0.032490 * abs(cos(cur_angle))
+  cur_b_theta = 0.040387 * sin(cur_angle)
+  cur_b_theta += 0.032490 * cos(cur_angle)
+
+  cur_b_theta = abs(cur_b_theta)
 
   cur_b_theta
 end
