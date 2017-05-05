77481c55c66cd6598ce1977a80649dc90f395fd6
Dan Segal <danSegal2@gmail.com>
2017-05-05 14:51:41 -0400

    Fix K_DV equation from a full rederive in Maple

--- src/functions/coeffs/K_DV.jl
+++ src/functions/coeffs/K_DV.jl
@@ -4,15 +4,18 @@
 Lorem ipsum dolor sit amet.
 """
 function K_DV()
-  cur_K_DV = 101.0
+  cur_K_DV = 4.0835e3
 
   cur_K_DV *= N_G ^ 2
-  cur_K_DV *= kappa ^ 0.5
+  cur_K_DV *= kappa ^ ( 3 // 2 )
+
+  cur_K_DV *= ( 5 + Q )
+  cur_K_DV /= ( 5 * Q )
 
   cur_K_DV *= ( 1 - rho_vol_loss )
 
   cur_kernel = N_G
-  cur_kernel *= kappa ^ 0.75
+  cur_kernel *= kappa ^ ( 3 // 4 )
   cur_kernel *= C_B()
 
   cur_kernel /= sqrt(epsilon)
@@ -23,7 +26,6 @@ function K_DV()
   cur_K_DV *= b_theta_target()
   cur_K_DV *= sin( divertor_beta )
 
-  cur_K_DV /= epsilon ^ 2
   cur_K_DV /= 1 + epsilon
 
   cur_K_DV
