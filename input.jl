4567fea1cb4a34256e03a6087bb4531ec079bff6
Dan Segal <danSegal2@gmail.com>
2017-05-07 16:12:22 -0400

    Move Arc input to its own file

--- input.jl
+++
@@ -1,9 +0,0 @@
-Q = 13.6
-kappa = 1.84
-H = 1.8
-epsilon = 0.34
-N_G = 0.7
-eta_CD = 0.32
-
-
-enable_bremsstrahlung = false
---
+++ lib/input_decks/arc.jl
@@ -0,0 +1,10 @@
+Q = 18.86
+kappa = 1.84
+H = 1.8
+epsilon = 0.342
+N_G = 0.7
+eta_CD = 0.32
+nu_n = 0.385
+nu_T = 0.929
+
+enable_bremsstrahlung = false
