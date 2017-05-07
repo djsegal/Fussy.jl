471a563833cf63e2938a646f95a9a54830d05d78
Dan Segal <danSegal2@gmail.com>
2017-05-07 16:11:23 -0400

    Add explicit dependence on K_CD_hat to K_F

--- src/functions/coeffs/K_CD_hat.jl
+++ src/functions/coeffs/K_CD_hat.jl
@@ -4,11 +4,9 @@
 Lorem ipsum dolor sit amet.
 """
 function K_CD_hat()
-  cur_K_CD = 278.3
+  cur_K_CD = K_F()
 
   cur_K_CD *= eta_CD
-  cur_K_CD *= epsilon^2
-  cur_K_CD *= kappa
 
   cur_K_CD /= Q
 
