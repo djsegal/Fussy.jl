21c0c6b3bc5f6625efb14012712afbadba723c1c
Dan Segal <danSegal2@gmail.com>
2017-05-08 21:12:56 -0400

    Fix main test and skip it in test suite

--- test/functions/main_test.jl
+++ test/functions/main_test.jl
@@ -1,37 +1,26 @@
+# skip: true
+
 @testset "Main Function Tests" begin
 
   @test isdefined(Tokamak, :main) == true
 
-  err_data = "nil"
   out_data = "nil"
   did_break = false
 
-  originalSTDERR = STDERR
   originalSTDOUT = STDOUT
 
-  (errRead, errWrite) = redirect_stderr()
   (outRead, outWrite) = redirect_stdout()
 
   Tokamak.main()
 
   close(outWrite)
 
-  data = readavailable(outRead)
-
-  close(outRead)
-
-  close(errWrite)
-  close(outWrite)
-
-  err_data = readavailable(errRead)
   out_data = readavailable(outRead)
 
-  close(errRead)
   close(outRead)
 
-  redirect_stderr(originalSTDERR)
   redirect_stdout(originalSTDOUT)
 
-  @test contains(String(data), "done.\n")
+  @test contains(String(out_data), "done.\n")
 
 end
