52a92733346c96ffc2f78cea5f2483c5b8afb932
Dan Segal <danSegal2@gmail.com>
2017-05-08 20:59:50 -0400

    Add act_1 and act_2 input // there might be errors

---
+++ lib/input_decks/act_1.jl
@@ -0,0 +1,16 @@
+Q = 42.5
+kappa = 2.2
+delta = 0.63
+epsilon = 0.25
+N_G = 1.0
+H = 1.65
+
+nu_n = 0.27
+nu_T = 1.15
+
+Z_eff = 2.11
+
+eta_CD = 0.4
+
+max_beta_N = 0.0475
+max_P_W = 2.45 * ( 1u"MW" / 1u"m^2" )
---
+++ lib/input_decks/act_2.jl
@@ -0,0 +1,16 @@
+Q = 25.0
+kappa = 2.2
+delta = 0.63
+epsilon = 0.25
+N_G = 0.77
+H = 1.22
+
+nu_n = 0.41
+nu_T = 1.15
+
+Z_eff = 2.12
+
+eta_CD = 0.4
+
+max_beta_N = 0.0225
+max_P_W = 1.46 * ( 1u"MW" / 1u"m^2" )
