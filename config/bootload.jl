d0d6210ed4b25a4b2fef0f55c106901d881d0e33
Dan Segal <danSegal2@gmail.com>
2017-05-12 17:06:22 -0400

    Update bootload script to improve usage robustness

--- config/bootload.jl
+++ config/bootload.jl
@@ -1,17 +1,23 @@
-main_folder = pwd()
+main_folder = "$(dirname(@__FILE__))/.."
 
 loaded_folders = [ "config/initializers", "src" ]
 
 for loaded_folder in loaded_folders
   loaded_folder = "$main_folder/$loaded_folder"
 
-  Julz.include_all_files(loaded_folder)
+  Julz.include_all_files(loaded_folder, package_name="Tokamak")
   @eval Julz.@export_all_files $loaded_folder
 end
 
-function load_input(raw_input, is_file=false)
-  if is_file
-    open("$main_folder/$raw_input") do file
+function load_input(raw_input, is_file_input=false)
+  if is_file_input
+    file_path = "$main_folder/$raw_input"
+
+    if !isfile(file_path)
+      file_path = "$main_folder/lib/input_decks/$raw_input"
+    end
+
+    open(file_path) do file
       file_lines = map(x -> split(x, "#")[1], readlines(file))
       file_lines = map(x -> strip(x), file_lines)
 
@@ -19,6 +25,8 @@ function load_input(raw_input, is_file=false)
 
       raw_input = replace(raw_input, "(;", "(")
       raw_input = replace(raw_input, ",;", ",")
+
+      raw_input *= "; nothing ;"
     end
   end
 
