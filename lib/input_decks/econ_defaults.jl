6000876e7a97fec9c95de0fc7bee6db8949bb180
Dan Segal <danSegal2@gmail.com>
2017-05-18 17:06:49 -0400

    Add lcoe’s for various power production techniques

--- lib/input_decks/econ_defaults.jl
+++ lib/input_decks/econ_defaults.jl
@@ -2,4 +2,10 @@ cost_W = 600000 # USD/m^3
 cost_steel = 16000 # USD/m^3
 cost_inconel = 66400 # USD/m^3
 
+lcoe_wind = 73.6
+lcoe_com_gas = 75.2
+lcoe_coal = 95.1
+lcoe_fission = 95.2
+lcoe_solar = 125.3
+
 N_cl = 26 # number of current leads (constant)
