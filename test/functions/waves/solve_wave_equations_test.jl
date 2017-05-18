0b119f9375cf26f9721e409b51e18d8c50ade70c
Dan Segal <danSegal2@gmail.com>
2017-05-18 02:47:35 -0400

    Add test for solving the wave equation for eta_CD

--- test/functions/waves/solve_wave_equations_test.jl
+++ test/functions/waves/solve_wave_equations_test.jl
@@ -2,4 +2,21 @@
 
   @test isdefined(Tokamak, :solve_wave_equations) == true
 
+  Tokamak.load_input(" Z_eff = 1.0 ")
+  Tokamak.load_input(" n_bar = 0.86 * 1u\"n20\" ")
+  Tokamak.load_input(" T_k = 17.8 * 1u\"keV\" ")
+
+  cur_solved_R_0 = 4.0
+  cur_solved_B_0 = 10.0
+
+  actual_value = Tokamak.solve_wave_equations(cur_solved_R_0, cur_solved_B_0)
+
+  expected_value = [1.273554655214011, 0.699050110239589, 1.464955718931050e+10]
+
+  @test length(actual_value) == length(expected_value)
+
+  for cur_index in 1:length(expected_value)
+    @test isapprox(expected_value[cur_index], actual_value[cur_index], rtol=5e-4)
+  end
+
 end
