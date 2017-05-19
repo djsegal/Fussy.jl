959ee1fcd75773562ff16b6f07c95f3aa16ee7c1
Dan Segal <danSegal2@gmail.com>
2017-05-19 15:04:59 -0400

    Add default variables for econ code

--- lib/input_decks/econ_defaults.jl
+++ lib/input_decks/econ_defaults.jl
@@ -9,3 +9,9 @@ lcoe_fission = 95.2
 lcoe_solar = 125.3
 
 N_cl = 26 # number of current leads (constant)
+
+plant_life_in_years = 50 # lifetime of plant from start of construction to end of decomissioning
+construction_time_in_years = 7 # construction time
+discount_rate = 8 // 100 # discount rate for the project
+econ_availability = 0.7
+
