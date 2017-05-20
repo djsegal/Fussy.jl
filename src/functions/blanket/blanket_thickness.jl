e74ab96b55b67567078c6d6e59ee3ff05143f820
Dan Segal <danSegal2@gmail.com>
2017-05-19 21:24:23 -0400

    Add description to blanket_thickness.jl

--- src/functions/blanket/blanket_thickness.jl
+++ src/functions/blanket/blanket_thickness.jl
@@ -1,7 +1,8 @@
 """
     blanket_thickness()
 
-Lorem ipsum dolor sit amet.
+Total thickness of everything between plasma and magnets, ie:
+first wall, vacuum vessel, flibe blanket, be multiplier, and shielding..
 """
 function blanket_thickness()
 
