82d10ad74f68babe604ba317138753a139a2726a
Dan Segal <danSegal2@gmail.com>
2017-05-20 21:59:10 -0400

    Update surface area to not calc the crossSect area

--- src/functions/plasmas/surface_area.jl
+++ src/functions/plasmas/surface_area.jl
@@ -4,13 +4,44 @@
 Lorem ipsum dolor sit amet.
 """
 function surface_area(cur_R_0=R_0)
-  cur_surface_area = geometry_scaling()
 
-  cur_surface_area *= pi
+  cur_c_part = 1.0
 
-  cur_surface_area *= a(cur_R_0) ^ 2
+  cur_c_part -= 2 * delta
+  cur_c_part -= x_aa_of_pi()
 
-  cur_surface_area *= kappa
+  cur_c_part /= 8.0
+
+  cur_c_array = [
+    c0 = -delta / 2.0
+    c1 = 1.0 + cur_c_part
+    c2 = +delta / 2.0
+    c3 = 0.0 - ( 1 - ( 2 * delta + x_aa_of_pi() ) ) / 8.0
+  ]
+
+  cur_func = function (cur_t)
+    dydt = kappa * cos(cur_t)
+
+    dxdt = 0.0
+
+    for cur_index in 0:3
+      cur_dxdt_part = cur_index
+      cur_dxdt_part *= cur_c_array[cur_index+1]
+      cur_dxdt_part *= sin(cur_index * cur_t)
+
+      dxdt -= cur_dxdt_part
+    end
+
+    cur_value = sqrt( dxdt^2 + dydt^2 )
+
+    cur_value
+  end
+
+  cur_surface_area = QuadGK.quadgk( cur_func , 0 , (2*pi) )[1]
+
+  cur_surface_area *= 1u"m"
+
+  cur_surface_area *= 2 * pi * cur_R_0
 
   cur_surface_area
 end
