b09d9ce610e05353e3e795b7001fca6fa804c234
Dan Segal <danSegal2@gmail.com>
2017-05-22 03:13:40 -0400

    Fix bug where betap was using wrong nu global vars

--- src/functions/magnets/betap.jl
+++ src/functions/magnets/betap.jl
@@ -4,19 +4,73 @@
 Lorem ipsum dolor sit amet.
 """
 function betap()
-  Bta = standard_mu_0*( I_M / 1u"A" )/(2*pi*( a() / 1u"m" ))
+
+  # Bta Calculation
+
+  Bta = standard_mu_0
+
+  Bta *= ( I_M / 1u"A" )
+
+  Bta /= 2 * pi
+
+  Bta /= ( a() / 1u"m" )
+
   # Pressure Calculation
-  nt=1.3
-  nn = 0.5
-  sigma=0.1
-  xaa=0.3*(1-delta^2)
-  c0 = -delta/2
-  c1 = 1/8*(9-2*delta-xaa)
-  c2 = delta/2
-  c3 = 1/8*(-1+2*delta+xaa)
-  press = 2*( n_bar / 1u"m^-3" )*( ( T_k / 1u"eV" ) * 11600 )*( Unitful.k / 1u"J/K" )*(1+nn)*(1+nt)/(1+nn+nt)*(c1 + ( a() / 1u"m" )/( R_0 / 1u"m" )*(6*c0*c1+3*c2*c3+c1*
-     (3+nn+nt)*(c2+sigma*(nn+nt)))/((2+nn+nt)*(3+nn+nt))) |> NoUnits
-  cur_betap = 2*standard_mu_0*press/(Bta^2)
+
+  cur_shape_array = d_shape_array()
+
+  press = shape_sigma
+
+  press *= ( nu_n + nu_T )
+
+  press += cur_shape_array[3]
+
+  press *= cur_shape_array[2]
+
+  press *= ( 3 + nu_n + nu_T )
+
+  press += 6 * cur_shape_array[1] * cur_shape_array[2]
+
+  press += 3 * cur_shape_array[3] * cur_shape_array[4]
+
+  press *= ( a() / 1u"m" )
+
+  press /= ( R_0 / 1u"m" )
+
+  press /= ( 2 + nu_n + nu_T )
+
+  press /= ( 3 + nu_n + nu_T )
+
+  press += cur_shape_array[2]
+
+  press *= 2
+
+  press *= ( n_bar / 1u"m^-3" )
+
+  press *= ( T_k / 1u"eV" )
+
+  press *= 11600
+
+  press *= ( Unitful.k / 1u"J/K" )
+
+  press *= ( 1 + nu_n )
+
+  press *= ( 1 + nu_T )
+
+  press /= ( 1 + nu_n + nu_T )
+
+  press = press |> NoUnits
+
+  # betap Calculation
+
+  cur_betap = 2
+
+  cur_betap *= standard_mu_0
+
+  cur_betap *= press
+
+  cur_betap /= Bta ^ 2
 
   cur_betap
+
 end
--- test/functions/magnets/magnet_kp_test.jl
+++ test/functions/magnets/magnet_kp_test.jl
@@ -5,6 +5,8 @@
   Tokamak.load_input(" I_M = 8 * 1u\"MA\" ")
   Tokamak.load_input(" T_k = 15 * 1u\"keV\" ")
   Tokamak.load_input(" n_bar = 1.5 * 1u\"n20\" ")
+  Tokamak.load_input(" nu_T = 1.3 ")
+  Tokamak.load_input(" nu_n = 0.5 ")
   Tokamak.load_input(" delta = 0.45 ")
   Tokamak.load_input(" epsilon = 0.3424242424 ")
   Tokamak.load_input(" enable_blanket_derive = false ")
