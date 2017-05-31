dd10551d5484d1ef4092f9fa660d6da7aad325f7
Dan Segal <danSegal2@gmail.com>
2017-05-31 17:08:31 -0400

    Refactor Cryostat_Cost & magnet_Q_80 so they match

--- src/functions/econ/Cryostat_Cost.jl
+++ src/functions/econ/Cryostat_Cost.jl
@@ -4,5 +4,17 @@
 Cost of cryostat scaled to ITER.
 """
 function Cryostat_Cost()
-  (75.8*10^6)*((1+kappa^2)*( R_0 / 1u"m" )*( a() / 1u"m" ))/48.2
+
+  cur_Cryostat_Cost = 75.8e6
+
+  cur_Cryostat_Cost *= 1 + kappa ^ 2
+
+  cur_Cryostat_Cost *= ( R_0 / 1u"m" )
+
+  cur_Cryostat_Cost *= ( a() / 1u"m" )
+
+  cur_Cryostat_Cost /= 48.2
+
+  cur_Cryostat_Cost
+
 end
--- src/functions/magnets/magnet_Q_80.jl
+++ src/functions/magnets/magnet_Q_80.jl
@@ -5,13 +5,13 @@ Heating due to thermal shielding @ 80K [kW].
 """
 function magnet_Q_80()
 
-  cur_Q_80 = 1 + kappa ^ 2
+  cur_Q_80 = 800
+
+  cur_Q_80 *= 1 + kappa ^ 2
 
   cur_Q_80 *= ( R_0 / 1u"m" )
 
   cur_Q_80 *= ( a() / 1u"m" )
-
-  cur_Q_80 *= 800
 
   cur_Q_80 /= 48.2
 
