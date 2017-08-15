97df555607fd857e40f52b9d98542b3c8e7af1ae
Dan Segal <danSegal2@gmail.com>
2017-08-15 19:52:49 -0400

    Add normalization to solved_T_k equations

--- src/functions/solved/solved_T_k_from_beta.jl
+++ src/functions/solved/solved_T_k_from_beta.jl
@@ -13,7 +13,9 @@ function solved_T_k_from_beta(T_guess, cur_B_0=( B_0 / 1u"T" ), cur_eta_CD=defau
 
   cur_eq /= K_1() * G_1()
 
-  cur_eq -= cur_B_0 ^ ( alphas["B_0"] + cur_eq_exp )
+  cur_eq /= cur_B_0 ^ ( alphas["B_0"] + cur_eq_exp )
+
+  cur_eq -= 1.0
 
   solved_T_k = solved_T_k_from_constraint(T_guess, cur_eta_CD, cur_eq, verbose)
 
