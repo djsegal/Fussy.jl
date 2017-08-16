51bfd7ead3c34e938c3c54a315667683f297aa22
Dan Segal <danSegal2@gmail.com>
2017-08-15 20:57:46 -0400

    Speed up T_k solve by quitting on repeat bad T_k

--- src/functions/solved/solved_T_k_from_constraint.jl
+++ src/functions/solved/solved_T_k_from_constraint.jl
@@ -16,6 +16,10 @@ function solved_T_k_from_constraint(T_guess, cur_eta_CD, cur_eq, verbose)
 
   did_work = true
 
+  cur_error = Inf
+
+  prev_T_k = 0.0
+
   for cur_T_attempt in T_attempt_list
     did_work = true
 
@@ -25,15 +29,21 @@ function solved_T_k_from_constraint(T_guess, cur_eta_CD, cur_eq, verbose)
         show_trace = false, xtol = ( wave_error_level / 10 ), iterations = 250
       ).zero[1]
 
-      cur_F = calc_possible_values(
+      cur_error = calc_possible_values(
         cur_eq,
         cur_T_k=solved_T_k*1u"keV",
         cur_eta_CD=cur_eta_CD
       )
 
-      if !isreal(solved_T_k) || abs(cur_F) > wave_error_level
+      if !isreal(solved_T_k) || abs(cur_error) > wave_error_level
         did_work = false
       end
+
+      if isapprox(prev_T_k, solved_T_k, rtol=wave_error_level/10)
+        break
+      end
+
+      prev_T_k = solved_T_k
     catch DomainError
       did_work = false
     end
