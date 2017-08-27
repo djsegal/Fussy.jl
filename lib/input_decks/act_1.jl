c31da2d28f84e42eb6ec9fd57ba90ba25d3ddf0b
Dan Segal <danSegal2@gmail.com>
2017-08-27 15:07:32 -0400

    Improve act_1 & act_2 input decks w/ learned info

--- lib/input_decks/act_1.jl
+++ lib/input_decks/act_1.jl
@@ -1,8 +1,8 @@
 # act 1
 
 Q = 42.5
-kappa = 2.2
-delta = 0.63
+kappa = 2.0
+delta = 0.575
 epsilon = 0.25
 N_G = 1.0
 H = 1.65
@@ -10,10 +10,19 @@ H = 1.65
 nu_n = 0.27
 nu_T = 1.15
 
-default_eta_CD = 0.4
+default_eta_CD = 0.188
 
 Z_eff = 2.11
 f_DT = 0.75
 
-max_beta_N = 4.75/100 # 0.056
+max_beta_N = 0.056
 max_P_W = 2.45
+
+wave_theta = 150.0
+
+eta_T = 0.575
+eta_RF = 0.4
+
+rho_m = 0.7
+
+default_constraint = "wall"
