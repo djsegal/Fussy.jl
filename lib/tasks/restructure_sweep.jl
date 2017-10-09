b342ce8d14ba589bcf517c7dbfadaf91ab92acfa
Dan Segal <danSegal2@gmail.com>
2017-10-08 23:22:12 -0400

    Add utility task for restructuring sweeps

---
+++ lib/tasks/restructure_sweep.jl
@@ -0,0 +1,30 @@
+"""
+    restructure_sweep()
+
+Lorem ipsum dolor sit amet.
+"""
+function restructure_sweep(old_sweep)
+  num_runs = length(first(values(old_sweep)))
+
+  new_sweep = Array{Any}(num_runs)
+
+  for cur_index in 1:length(new_sweep)
+    new_sweep[cur_index] = OrderedDict()
+
+    for (cur_key, cur_value) in old_sweep
+      if eltype(cur_value) == Pair{Any,Any}
+        new_sweep[cur_index][cur_key] = OrderedDict()
+
+        for (cur_sub_key, cur_sub_value) in cur_value
+          new_sweep[cur_index][cur_key][cur_sub_key] = cur_sub_value[cur_index]
+        end
+
+        continue
+      end
+
+      new_sweep[cur_index][cur_key] = cur_value[cur_index]
+    end
+  end
+
+  new_sweep
+end
