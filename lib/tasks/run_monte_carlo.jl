bef197c2f5e3a73116ae272eb58d64fa64a8133b
Dan Segal <danSegal2@gmail.com>
2017-10-13 16:21:59 -0400

    Add sensitivity study option to run_monte_carlo

--- lib/tasks/run_monte_carlo.jl
+++ lib/tasks/run_monte_carlo.jl
@@ -1,21 +1,27 @@
 """
-    run_monte_carlo(T_list, run_count, swept_params...; verbose=true)
+    run_monte_carlo(cur_T, run_count, swept_params...; cur_sensitivity=0.2, sensitive_variables=[], verbose=true)
 
 Lorem ipsum dolor sit amet.
 """
-function run_monte_carlo(T_list, run_count, swept_params...; verbose=true)
-  if isempty(swept_params)
-    error("Monte Carlo requires swept variables")
-  end
-
-  cur_variable, cur_range = first(swept_params)
-
+function run_monte_carlo(cur_T, run_count, swept_params...; cur_sensitivity=0.2, sensitive_variables=[], verbose=true)
   cur_output = Array{Any}(run_count)
+
+  sensitive_params = OrderedDict()
+
+  for cur_param in sensitive_variables
+    if cur_param == "T_k"
+      sensitive_params[cur_param] = cur_T
+      continue
+    end
+
+    sensitive_params[cur_param] = getfield( Tokamak, Symbol(cur_param) )
+  end
 
   for cur_index in 1:run_count
     if verbose ; print(".") ; end
 
     loaded_input = ""
+
     for (cur_param, cur_range) in swept_params
       cur_diff = first(diff(cur_range))
 
@@ -26,9 +32,24 @@ function run_monte_carlo(T_list, run_count, swept_params...; verbose=true)
       loaded_input *= "$(cur_param) = $(cur_value);"
     end
 
+    for (cur_param, cur_value) in sensitive_params
+      cur_delta = cur_sensitivity * cur_value
+      cur_scale = 2 * rand() - 1.0
+
+      mutated_value = cur_value
+      mutated_value += cur_scale * cur_delta
+
+      if cur_param == "T_k"
+        cur_T = mutated_value
+        continue
+      end
+
+      loaded_input *= "$(cur_param) = $(mutated_value);"
+    end
+
     Tokamak.load_input(loaded_input)
 
-    cur_sweep = sweep_T_k(T_list; verbose=false)
+    cur_sweep = sweep_T_k([cur_T]; verbose=false)
 
     for (cur_key, cur_value) in cur_sweep
       if eltype(cur_value) == Pair{Any,Any}
