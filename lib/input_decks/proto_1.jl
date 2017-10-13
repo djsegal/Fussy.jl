815e71502180e9e17feda430155decbfcc16889d
Dan Segal <danSegal2@gmail.com>
2017-10-13 16:32:23 -0400

    Add proto_1 reactor to input_decks

---
+++ lib/input_decks/proto_1.jl
@@ -0,0 +1,25 @@
+# proto_1
+
+Q = 25.0
+kappa = 1.8
+# delta
+epsilon = 0.25
+N_G = 0.9
+H = 1.0
+
+nu_n = 0.4
+nu_T = 1.2
+
+default_eta_CD = 0.25
+
+Z_eff = 1.5
+f_DT = 0.9
+
+max_beta_N = 0.026
+max_P_W = 2.5
+
+wave_theta = 0.0
+
+rho_m = 0.7
+
+enable_hard_errors = false
