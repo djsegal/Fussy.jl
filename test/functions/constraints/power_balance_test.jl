1b8763a1d2e29c6c3386e4b6026b0aea4e78147e
djsegal <dansegal2@gmail.com>
2017-11-16 19:43:34 -0500

    wip

--- test/functions/constraints/power_balance_test.jl
+++
@@ -1,7 +0,0 @@
-@testset "Power Balance Function Tests" begin
-
-  @test isdefined(Tokamak, :power_balance) == true
-
-  @test Tokamak.power_balance() != Nullable
-
-end
--- test/functions/solved/solved_R_0_from_B_0_test.jl
+++
@@ -1,5 +0,0 @@
-@testset "Solved R 0 From B 0 Function Tests" begin
-
-  @test isdefined(Tokamak, :solved_R_0_from_B_0) == true
-
-end
