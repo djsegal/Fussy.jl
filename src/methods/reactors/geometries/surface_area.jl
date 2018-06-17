53b9165c4d10b8fcd2763a024b14eb8c9a6e349f
djsegal <dansegal2@gmail.com>
2018-06-17 03:21:21 -0400

    Update surface_area calculation

--- src/methods/reactors/geometries/surface_area.jl
+++ src/methods/reactors/geometries/surface_area.jl
@@ -1,15 +1,23 @@
 @symbol_func function surface_area(cur_reactor::AbstractReactor)
-  cur_area = 4.0
+  cur_left_part = 4.0
 
-  cur_area *= cur_reactor.pi ^ 2
+  cur_left_part *= cur_reactor.pi ^ 2
 
-  cur_area *= a(cur_reactor)
+  cur_left_part *= a(cur_reactor)
 
-  cur_area *= cur_reactor.R_0
+  cur_left_part *= cur_reactor.R_0
 
-  cur_area *= sqrt( 1 + cur_reactor.kappa_95 ^ 2 )
+  cur_right_part = ( cur_reactor.kappa_95 ^ 2 - 1 )
 
-  cur_area /= sqrt(2)
+  cur_right_part *= ( 2 / cur_reactor.pi )
+
+  cur_right_part += 1
+
+  cur_right_part /= cur_reactor.kappa_95
+
+  cur_area = cur_left_part
+
+  cur_area *= cur_right_part
 
   cur_area
 end
