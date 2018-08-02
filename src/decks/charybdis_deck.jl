3bea806ee45e982016e13ad1f3538e6be677608f
djsegal <dansegal2@gmail.com>
2018-08-02 00:09:32 -0400

    Split proto deck into pulsed and steady state vers

---
+++ src/decks/charybdis_deck.jl
@@ -0,0 +1,40 @@
+function charybdis_deck(cur_T_bar::AbstractSymbol=symbols(:T_bar); cur_kwargs...)
+  cur_dict = Dict(
+    :T_bar => cur_T_bar,
+    :is_pulsed => false,
+    :H => 1.6296,
+    :Q => 25.0,
+    :epsilon => 0.3,
+    :kappa_95 => 1.8,
+    :delta_95 => 0.35,
+    :nu_n => 0.4,
+    :nu_T => 1.1,
+    :Z_eff => 1.75,
+    :f_D => 0.9,
+    :A => 2.5,
+    :l_i => 0.5922,
+    :N_G => 0.9,
+    :eta_T => 0.4,
+    :eta_RF => 0.5,
+    :tau_FT => 1.6e9,
+    :C_ejima => 0.3,
+    :max_beta_N => 0.028,
+    :max_q_95 => 2.5,
+    :max_P_E => 3000.0,
+    :max_P_W => 3.0,
+    :max_q_DV => 10.0,
+    :B_CS => 20.0,
+    :sigma_CS => 600,
+    :sigma_TF => 300,
+    :J_CS => 50,
+    :J_TF => 200,
+    :k => 0.4,
+    :eta_CD => 0.2814,
+    :wave_theta => 135.0,
+    :eta_LH => 0.6
+  )
+
+  merge!(cur_dict, Dict(cur_kwargs))
+
+  BaseReactor(cur_dict)
+end
--- src/decks/index.jl
+++ src/decks/index.jl
@@ -6,4 +6,5 @@ include("arc_deck.jl")
 include("act_1_deck.jl")
 include("act_2_deck.jl")
 
-include("proto_deck.jl")
+include("scylla_deck.jl")
+include("charybdis_deck.jl")
