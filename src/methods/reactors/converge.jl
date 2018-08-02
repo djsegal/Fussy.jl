2a1a018d5a52113c3f50f8370a98164247363db1
djsegal <dansegal2@gmail.com>
2018-08-02 00:16:39 -0400

    Make converge method more flexible // i.e. no_pts

--- src/methods/reactors/converge.jl
+++ src/methods/reactors/converge.jl
@@ -1,22 +1,26 @@
 function converge(cur_T_bar::AbstractFloat, tmp_dict::Dict)
-  tmp_reactor = Reactor(cur_T_bar, tmp_dict)
-  tmp_reactor.T_bar <= 0 && return []
+  cur_reactor = Reactor(cur_T_bar, tmp_dict)
+  cur_reactor.T_bar <= 0 && return []
 
-  tmp_reactor.sigma_v = calc_sigma_v(tmp_reactor)
-  isnan(tmp_reactor.sigma_v) && return []
+  cur_reactor.sigma_v = calc_sigma_v(cur_reactor)
+  isnan(cur_reactor.sigma_v) && return []
 
+  converge(cur_reactor)
+end
+
+function converge(cur_reactor::AbstractReactor; no_pts=no_pts_eta_CD)
   cur_func = function (cur_eta_CD::Number)
     cur_eta_CD = real(cur_eta_CD)
 
-    cur_reactor = Reactor(cur_T_bar, tmp_dict)
+    tmp_reactor = deepcopy(cur_reactor)
 
-    cur_reactor.eta_CD = cur_eta_CD
+    tmp_reactor.eta_CD = cur_eta_CD
 
-    solve!(cur_reactor)
+    solve!(tmp_reactor)
 
-    cur_reactor.is_good || return Inf
+    tmp_reactor.is_good || return Inf
 
-    tmp_eta_CD = calc_eta_CD(cur_reactor)
+    tmp_eta_CD = calc_eta_CD(tmp_reactor)
 
     isnan(tmp_eta_CD) && return Inf
 
@@ -27,7 +31,7 @@ function converge(cur_T_bar::AbstractFloat, tmp_dict::Dict)
 
   cur_root_list = find_roots(
     cur_func, min_eta_CD, max_eta_CD,
-    no_pts = no_pts_eta_CD, abstol = 1e-4
+    no_pts = no_pts, abstol = 1e-4
   )
 
   cur_root_list
