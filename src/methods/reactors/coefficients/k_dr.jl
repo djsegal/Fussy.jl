b0cd5821a1545382a4ae0508389dd6e7f30f6f67
djsegal <dansegal2@gmail.com>
2018-08-19 01:08:52 -0400

    Ignore central solenoid width for steady-state

--- src/methods/reactors/coefficients/k_dr.jl
+++ src/methods/reactors/coefficients/k_dr.jl
@@ -1,4 +1,6 @@
 @symbol_func function K_DR(cur_reactor::AbstractReactor)
+  cur_reactor.is_pulsed || return 0.0
+
   cur_denominator = cur_reactor.sigma_CS
 
   cur_denominator *= (12/5) * cur_reactor.pi
