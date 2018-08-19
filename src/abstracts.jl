9fee603569d6941196c80249e7bbc5f25f0524dd
djsegal <dansegal2@gmail.com>
2018-08-19 02:05:49 -0400

    Add calibration abstract type

--- src/abstracts.jl
+++ src/abstracts.jl
@@ -4,6 +4,7 @@ AbstractSymbol = Union{Real, SymEngine.Basic}
 AbstractKey = Union{Symbol, Void}
 
 abstract type AbstractCounterFunction end
+abstract type AbstractCalibration end
 abstract type AbstractSampling end
 abstract type AbstractReactor end
 abstract type AbstractStudy end
