280ed4286d62106b13bbb1c3b374e990e8a4bcd7
djsegal <dansegal2@gmail.com>
2018-08-19 01:21:01 -0400

    Add a counter function that counts function calls

--- src/abstracts.jl
+++ src/abstracts.jl
@@ -3,6 +3,7 @@ AbstractSymbol = Union{Real, SymEngine.Basic}
 
 AbstractKey = Union{Symbol, Void}
 
+abstract type AbstractCounterFunction end
 abstract type AbstractReactor end
 abstract type AbstractStudy end
 abstract type AbstractScan end
--- src/structs/index.jl
+++ src/structs/index.jl
@@ -1,3 +1,4 @@
 include("reactor.jl")
 include("scan.jl")
 include("study.jl")
+include("counter_function.jl")
