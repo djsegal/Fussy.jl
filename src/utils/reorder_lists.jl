a2821a76917f3ad63515dcfab2e17c36cd323e41
djsegal <dansegal2@gmail.com>
2018-08-19 01:10:02 -0400

    Tighten up some reorder list utility methods

--- src/utils/reorder_lists.jl
+++ src/utils/reorder_lists.jl
@@ -1,7 +1,7 @@
 function sort_lists!(main_list, other_lists...; cur_func::Function=identity)
   cur_indices = sortperm(main_list, by=cur_func)
 
-  cur_list = main_list[cur_indices]
+  cur_list = collect(main_list)[cur_indices]
   _arrange_lists!(main_list, other_lists, cur_indices)
   cur_list
 end
@@ -15,17 +15,19 @@ function shuffle_lists!(main_list, other_lists...)
 end
 
 function _arrange_lists!(main_list, other_lists, cur_indices)
-  cur_lists = Any[ collect(main_list) ]
+  cur_lists = Any[ isa(main_list, Vector) ? main_list : collect(main_list) ]
   isempty(other_lists) || append!(cur_lists, other_lists)
 
   for cur_list in cur_lists
       cur_list .= cur_list[cur_indices]
   end
+  main_list
 end
 
 function bisect_reorder!(cur_vector::Vector)
   cur_indices = breadth_indices(length(cur_vector))
   cur_vector .= cur_vector[cur_indices]
+  cur_vector
 end
 
 function breadth_indices(num_points::Number)
@@ -50,12 +52,14 @@ end
 function out_in_reorder!(cur_vector::Vector)
   cur_indices = centering_indices(length(cur_vector))
   cur_vector .= cur_vector[cur_indices]
+  cur_vector
 end
 
 function centering_indices(num_points::Number)
   cur_indices = []
 
   cur_half_point = Int(floor(num_points/2))
+
   for cur_index in 1:cur_half_point
     push!(cur_indices, cur_index)
     push!(cur_indices, num_points-(cur_index-1))
