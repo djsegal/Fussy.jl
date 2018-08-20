94c597dfd096d49d06db518c820eb46612a644d2
djsegal <dansegal2@gmail.com>
2018-08-19 20:10:50 -0400

    Add inclusive flag to is_inside method

--- src/utils/is_inside.jl
+++ src/utils/is_inside.jl
@@ -1,5 +1,11 @@
-function is_inside(cur_x::Number, cur_a::Number, cur_b::Number)
-  ( cur_a <= cur_x <= cur_b ) && return true
-  ( cur_a >= cur_x >= cur_b ) && return true
+function is_inside(cur_x::Number, cur_a::Number, cur_b::Number; inclusive::Bool=true)
+  if inclusive
+    ( cur_a <= cur_x <= cur_b ) && return true
+    ( cur_a >= cur_x >= cur_b ) && return true
+  else
+    ( cur_a < cur_x < cur_b ) && return true
+    ( cur_a > cur_x > cur_b ) && return true
+  end
+
   false
 end
