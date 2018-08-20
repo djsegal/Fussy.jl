b2850e8c1710ff1101ff4bf68417f023f711fb0d
djsegal <dansegal2@gmail.com>
2018-08-19 20:10:39 -0400

    Get romanize method working up to number 19

--- src/utils/romanize.jl
+++ src/utils/romanize.jl
@@ -1,10 +1,22 @@
 function romanize(cur_int::Integer)
-  @assert cur_int < 9
+  @assert cur_int < 19
 
-  ( cur_int < 4 ) && return "I" ^ cur_int
-  ( cur_int == 4 ) && return "IV"
+  if cur_int < 9
+    ( cur_int < 4 ) && return "I" ^ cur_int
+    ( cur_int == 4 ) && return "IV"
 
-  cur_string = "V"
-  cur_string *= "I" ^ ( cur_int - 5 )
-  cur_string
+    cur_string = "V"
+    cur_string *= "I" ^ ( cur_int - 5 )
+    cur_string
+  else
+    ( cur_int == 9 ) && return "IX"
+
+    cur_string = "X"
+    ( cur_int < 14 ) && return cur_string * ( "I" ^ ( cur_int - 10 ) )
+    ( cur_int == 14 ) && return "XIV"
+
+    cur_string *= "V"
+    cur_string *= "I" ^ ( cur_int - 5 - 10 )
+    cur_string
+  end
 end
