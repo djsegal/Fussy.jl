345b98441178e66da19a70726a2304a8a6d9d38c
djsegal <dansegal2@gmail.com>
2018-08-21 21:02:58 -0400

    Get G_PB working with new model for ohmic heating

--- src/methods/reactors/composites/g_pb.jl
+++ src/methods/reactors/composites/g_pb.jl
@@ -1,13 +1,17 @@
 @symbol_func function G_PB(cur_reactor::AbstractReactor)
   cur_numerator = cur_reactor.T_bar
 
-  cur_denominator = K_P(cur_reactor) * cur_reactor.sigma_v
+  cur_denominator = ( 1 - K_CD(cur_reactor) * cur_reactor.sigma_v ) / cur_reactor.T_bar
 
-  cur_denominator += K_omega(cur_reactor) / ( sqrt(cur_reactor.T_bar) ^ 3 )
+  cur_denominator -= K_BS(cur_reactor) / safe_symbol(cur_reactor, :I_P)
+
+  cur_denominator /= sqrt(cur_reactor.T_bar)
+
+  cur_denominator *= K_omega(cur_reactor)
+
+  cur_denominator += K_P(cur_reactor) * cur_reactor.sigma_v
 
   cur_denominator -= K_BR(cur_reactor) * sqrt(cur_reactor.T_bar)
-
-  ( cur_denominator > 0 ) || return NaN
 
   cur_denominator ^= ( 1 - cur_reactor.mode_scaling[:P] )
 
