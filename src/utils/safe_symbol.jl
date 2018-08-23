c65530e80242098940d64e8212387c805408efc8
djsegal <dansegal2@gmail.com>
2018-08-22 23:41:38 -0400

    Add another condition for grabbing in safe_symbol

--- src/utils/safe_symbol.jl
+++ src/utils/safe_symbol.jl
@@ -1,5 +1,9 @@
 function safe_symbol(cur_reactor::AbstractReactor, cur_field::Symbol)
   cur_value = getfield(cur_reactor, cur_field)
+
   isa(cur_value, SymEngine.Basic) || return cur_value
+
+  isempty(free_symbols(cur_value)) && return N(cur_value)
+
   return symbols(cur_field)
 end
