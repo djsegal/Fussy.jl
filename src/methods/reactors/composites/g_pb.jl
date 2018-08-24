3f25369e86542edb8d184ee86e08b73f92deccc5
djsegal <dansegal2@gmail.com>
2018-08-24 03:10:59 -0400

    Change scaling relation back to: P_L = P_H + P_OHM

--- src/methods/reactors/composites/g_pb.jl
+++ src/methods/reactors/composites/g_pb.jl
@@ -1,17 +1,20 @@
 @symbol_func function G_PB(cur_reactor::AbstractReactor)
-  cur_numerator = cur_reactor.T_bar
-
+  cur_numerator = ( 1 - K_CD(cur_reactor) * cur_reactor.sigma_v )
   cur_denominator = ( 1 - K_CD(cur_reactor) * cur_reactor.sigma_v )
 
+  cur_numerator /= sqrt(cur_reactor.T_bar) ^ 3
   cur_denominator /= sqrt(cur_reactor.T_bar) ^ 3
 
+  cur_numerator *= K_omega(cur_reactor)
   cur_denominator *= K_omega(cur_reactor)
 
+  cur_numerator += K_P(cur_reactor) * cur_reactor.sigma_v
   cur_denominator += K_P(cur_reactor) * cur_reactor.sigma_v
 
-  cur_denominator -= K_BR(cur_reactor) * sqrt(cur_reactor.T_bar)
+  cur_numerator ^= cur_reactor.mode_scaling[:P]
+  cur_numerator *= cur_reactor.T_bar
 
-  cur_denominator ^= ( 1 - cur_reactor.mode_scaling[:P] )
+  cur_denominator -= K_BR(cur_reactor) * sqrt(cur_reactor.T_bar)
 
   cur_G = cur_numerator
 
