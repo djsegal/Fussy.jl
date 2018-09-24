b5f23fc7d27d0b75d821595b675aae87cd5bfc7b
djsegal <dansegal2@gmail.com>
2018-09-24 14:26:50 -0400

    Update sampling to actually be useful (like study)

--- src/structs/sampling.jl
+++ src/structs/sampling.jl
@@ -3,22 +3,30 @@ mutable struct Sampling <: AbstractSampling
 
   sensitivity::Real
 
-  run_count::Int
-  max_reactors::Int
+  study_count::Int
 
   deck::Union{Void, Symbol}
 
   defaults::Vector{Real}
-  reactors::Vector{AbstractReactor}
+
+  kink_reactors::Vector{AbstractNullableReactor}
+  wall_reactors::Vector{AbstractNullableReactor}
+
+  cost_reactors::Vector{AbstractNullableReactor}
+  W_M_reactors::Vector{AbstractNullableReactor}
 end
 
-function Sampling(parameters::Vector{Symbol}; sensitivity::Real=1.0, run_count::Int=100, max_reactors::Int=25, deck::Union{Void, Symbol}=nothing, batch_count::Int=5, max_time::Number=NaN, cur_kwargs...)
+function Sampling(parameters::Vector{Symbol}; sensitivity::Real=0.35, study_count::Int=100, deck::Union{Void, Symbol}=nothing, is_parallel::Bool=true, cur_kwargs...)
   cur_dict = merge!(Dict(), Dict(cur_kwargs))
   cur_dict[:constraint] = :beta
   cur_dict[:deck] = deck
 
+  cur_reactor_types = [:kink, :wall, :cost,  :W_M]
+
   defaults = []
   ranges = OrderedDict()
+
+  reactors_per_study = 4
 
   work_reactor = Reactor(symbols(:T_bar), cur_dict)
 
@@ -32,22 +40,29 @@ function Sampling(parameters::Vector{Symbol}; sensitivity::Real=1.0, run_count::
   end
 
   cur_sampling = Sampling(
-    parameters, sensitivity, run_count, max_reactors, deck, defaults, []
+    parameters, sensitivity, study_count, deck, defaults,
+    [ fill!(Vector(study_count), nothing) for cur_index in 1:reactors_per_study]...
   )
 
   @assert !haskey(ranges, :T_bar)
 
-  cur_array = SharedArray{Float64}(max_reactors, 4+length(ranges))
+  cur_constraints = SharedArray{Int}(study_count, reactors_per_study)
+
+  cur_array = SharedArray{Float64}(study_count, reactors_per_study, 3+length(ranges))
   fill!(cur_array, NaN)
 
   cur_func = function (cur_index::Integer)
     tmp_dict = deepcopy(cur_dict)
 
-    tmp_array = Vector{Float64}(4+length(ranges))
+    tmp_array = Vector{Float64}(length(ranges))
 
     for (cur_sub_index, (cur_key, cur_value)) in enumerate(ranges)
       if isa(cur_value, Tuple)
-        tmp_value = rand(Uniform(cur_value...))
+        if length(unique(cur_value)) == 1
+          tmp_value = cur_value[1]
+        else
+          tmp_value = rand(Uniform(cur_value...))
+        end
       elseif isa(cur_value, AbstractArray)
         tmp_value = pluck(cur_value)
       elseif isa(cur_value, Number)
@@ -56,7 +71,7 @@ function Sampling(parameters::Vector{Symbol}; sensitivity::Real=1.0, run_count::
         error("Invalid sampling range for: $(cur_key)")
       end
 
-      tmp_array[4+cur_sub_index] = tmp_value
+      tmp_array[cur_sub_index] = tmp_value
       tmp_dict[cur_key] = tmp_value
     end
 
@@ -64,105 +79,57 @@ function Sampling(parameters::Vector{Symbol}; sensitivity::Real=1.0, run_count::
 
     tmp_reactor.is_good || return
 
-    for cur_limit in [:wall, :kink]
-      cur_reactor = nothing
+    tmp_study = Study(:H, tmp_dict; num_points=1, verbose=false, is_parallel=false)
 
-      if tmp_reactor.is_consistent
-        cur_reactor = hone(tmp_reactor, cur_limit)
-      else
-        cur_reactor = match(tmp_reactor, cur_limit)
-      end
+    for (cur_sub_index, cur_reactor_type) in enumerate(cur_reactor_types)
+      tmp_sub_reactors = getfield(tmp_study, Symbol("$(cur_reactor_type)_reactors"))
+      isempty(tmp_sub_reactors) && continue
 
-      ( cur_reactor == nothing ) && continue
-      ( cur_reactor.is_valid ) || continue
+      @assert length(tmp_sub_reactors) == 1
+      tmp_sub_reactor = tmp_sub_reactors[1]
 
-      tmp_array[1] = cur_reactor.T_bar
-      tmp_array[2] = cur_reactor.I_P
-      tmp_array[3] = cur_reactor.eta_CD
-      tmp_array[4] = cur_reactor.cost
+      tmp_sub_array = [
+        tmp_sub_reactor.T_bar,
+        tmp_sub_reactor.I_P,
+        tmp_sub_reactor.eta_CD
+      ]
 
-      needs_continue = false
-
-      cur_min = cur_reactor.cost
-      for cur_attempt in 1:5
-        cur_sub_index = pluck(1:max_reactors)
-        other_min = cur_array[cur_sub_index, 4]
-
-        ( isnan(other_min) || cur_min < other_min ) || continue
-
-        cur_array[cur_sub_index,:] = tmp_array
-        needs_continue = true
-        break
-      end
-
-      needs_continue && continue
-
-      cur_nan_list = find(isnan, cur_array[:,4])
-      if !isempty(cur_nan_list)
-        cur_sub_index = pluck(cur_nan_list)
-        cur_array[cur_sub_index,:] = tmp_array
-        continue
-      end
-
-      cur_min_list = find(
-        other_min -> ( cur_min < other_min ),
-        cur_array[:,4]
-      )
-
-      isempty(cur_min_list) && continue
-      cur_sub_index = pluck(cur_min_list)
-      cur_array[cur_sub_index,:] = tmp_array
+      cur_array[cur_index, cur_sub_index, :] = [ tmp_sub_array..., tmp_array... ]
+      cur_constraints[cur_index, cur_sub_index] =
+        findfirst(tmp_constraint -> tmp_constraint == tmp_sub_reactor.constraint, collect(keys(secondary_limits)))
     end
 
     return
   end
 
-  run_per_batch = Int(floor( run_count / batch_count ))
-  extra_run_count = ( run_count - batch_count * run_per_batch )
-
-  cur_pmap_complete = SharedArray{Bool}(batch_count)
-
-  for cur_batch in 1:batch_count
-    cur_run_count = run_per_batch
-    ( cur_batch <= extra_run_count ) && ( cur_run_count += 1 )
-
-     if !isnan(max_time)
-      @async begin
-        sleep(max_time)
-        cur_pmap_complete[cur_batch] || interrupt(workers())
-      end
-    end
-
-    cur_progress = Progress(cur_run_count)
-
-    try
-      pmap(cur_func, cur_progress, 1:cur_run_count)
-    catch cur_error
-      isa(cur_error, RemoteException) || rethrow(cur_error)
-
-      sleep(4)
-      finish!(cur_progress)
-    end
-
-    cur_pmap_complete[cur_batch] = true
+  if is_parallel
+    cur_progress = Progress(study_count)
+    pmap(cur_func, cur_progress, shuffle(1:study_count))
+  else
+    map(cur_func, 1:study_count)
   end
 
-  for cur_index in 1:max_reactors
-    any(isnan, cur_array[cur_index,:]) && continue
+  for cur_index in 1:study_count
+    for (cur_sub_index, cur_reactor_type) in enumerate(cur_reactor_types)
+      any(isnan, cur_array[cur_index,cur_sub_index,:]) && continue
 
-    cur_T_bar, cur_I_P, cur_eta_CD = cur_array[cur_index,1:3]
+      cur_T_bar, cur_I_P, cur_eta_CD = cur_array[cur_index,cur_sub_index,1:3]
 
-    tmp_dict = deepcopy(cur_dict)
+      tmp_dict = deepcopy(cur_dict)
 
-    for (cur_sub_index, cur_range_name) in enumerate(keys(ranges))
-      tmp_dict[cur_range_name] = cur_array[cur_index, cur_sub_index+4]
+      for (cur_sub_sub_index, cur_range_name) in enumerate(keys(ranges))
+        tmp_dict[cur_range_name] = cur_array[cur_index, cur_sub_index, cur_sub_sub_index+3]
+      end
+
+      tmp_dict[:constraint] = collect(keys(secondary_limits))[cur_constraints[cur_index,cur_sub_index]]
+
+      work_reactor = Reactor(cur_T_bar, tmp_dict)
+      work_reactor.I_P = cur_I_P
+      work_reactor.eta_CD = cur_eta_CD
+
+      work_reactors = getfield(cur_sampling, Symbol("$(cur_reactor_type)_reactors"))
+      work_reactors[cur_index] = update!(work_reactor)
     end
-
-    work_reactor = Reactor(cur_T_bar, tmp_dict)
-    work_reactor.I_P = cur_I_P
-    work_reactor.eta_CD = cur_eta_CD
-
-    push!(cur_sampling.reactors, update!(work_reactor))
   end
 
   cur_sampling
