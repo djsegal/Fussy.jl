fe0d0872aad3f2d43f5956eb061b320cc659292a
djsegal <dansegal2@gmail.com>
2018-09-24 14:06:48 -0400

    Add is_finite method to stand aside is_nan

--- src/utils/is_nan.jl
+++ src/utils/is_nan.jl
@@ -5,3 +5,12 @@ function is_nan(cur_value::SymEngine.Basic)
 
   isnan(N(cur_value))
 end
+
+is_finite(cur_value::Number) = isfinite(cur_value)
+
+function is_finite(cur_value::SymEngine.Basic)
+  isempty(free_symbols(cur_value)) || return true
+  is_nan(cur_value) && return false
+
+  isfinite(N(cur_value))
+end
